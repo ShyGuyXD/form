8339009d50c960a69970a6e41a36453ece2a37cc
ShyGuyXD <emeljanenko.matvej2005@gmail.com>
2025-05-20 12:02:11 +0300

    Add files via upload

---
+++ form/private/functions/validator.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'autoload.php';
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $db = new Database();
+    $validator = new Validator($db->conn);
+    
+    if ($validator->validate($_POST)) {
+        if ($validator->save($_POST['fio'], $_POST['email'], $_POST['message'])) {
+            echo "Данные успешно сохранены!";
+        } else {
+            echo "Произошла ошибка, пожалуйста, повторите попытку.";
+        }
+    } else {
+        echo implode("\n", $validator->errors);
+    }
+
+    $db->close();
+}

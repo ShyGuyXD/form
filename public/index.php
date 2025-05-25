e65681e7fa78fb57636e9d42205cc94178f988fa
ShyGuyXD <emeljanenko.matvej2005@gmail.com>
2025-05-25 14:41:39 +0300

    i

--- public/index.php
+++ public/index.php
@@ -13,5 +13,5 @@ $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
-include 'index.html';
+
 ?>
--- src/Controller/HeheController.php
+++ src/Controller/HeheController.php
@@ -6,37 +6,46 @@ use App\classes\Validator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class HeheController extends AbstractController
+{   
+    /**
+     * @Route("/api/localhost", name="localhost", methods={"POST", "OPTIONS"})
+     */
+     public function index(): Response
 {
+$path = "index.html";
+return new Response($path);
+}
     /**
      * @Route("/api/submit-form", name="submit_form", methods={"POST", "OPTIONS"})
      */
    public function submitForm(Request $request)
    {
        $response = new JsonResponse();
-       $response->headers->set('Access-Control-Allow-Origin', '*');
+      /*$response->headers->set('Access-Control-Allow-Origin', '*');
        $response->headers->set('Access-Control-Allow-Methods', 'POST, OPTIONS');
-       $response->headers->set('Access-Control-Allow-Headers', 'Content-Type');
+       $response->headers->set('Access-Control-Allow-Headers', 'Content-Type');*/
 
        if ($request->getMethod() === 'OPTIONS') {
            return $response->setStatusCode(204);
        }
 
        $data = json_decode($request->getContent(), true);
-       if (empty($data)) {
+      /* if (empty($data)) {
            return $this->createCorsResponse(['error' => 'Нет данных.'], 400);
-       }
+       }*/
 
-       $database = new Database();
+       /*$database = new Database();
        $connection = $database->conn;
-
+       
        $validator = new Validator($connection);
 
-       if (!$validator->validate($data)) {
+       /*if (!$validator->validate($data)) {
            return $this->createCorsResponse(['errors' => $validator->errors], 400);
-       }
+       }*/
 
        $name = $data['fio'];
        $email = $data['email'];
@@ -45,12 +54,12 @@ class HeheController extends AbstractController
        return $response->setData(['message' => 'Данные успешно сохранены!'])->setStatusCode(200);
    }
 
-   private function createCorsResponse($data, $status = 200)
+   /*private function createCorsResponse($data, $status = 200)
    {
        $response = new JsonResponse($data, $status);
        $response->headers->set('Access-Control-Allow-Origin', '*');
        $response->headers->set('Access-Control-Allow-Methods', 'POST, OPTIONS');
        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type');
        return $response;
-   }
+   }*/
 }

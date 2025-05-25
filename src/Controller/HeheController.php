8b4e6b55f5a18514ca2e1e56b9f5b2a35010e486
ShyGuyXD <emeljanenko.matvej2005@gmail.com>
2025-05-25 19:35:10 +0300

    j

--- src/Controller/HeheController.php
+++ src/Controller/HeheController.php
@@ -1,22 +1,32 @@
 <?php
 namespace App\Controller;
-
+ 
 use App\classes\Database;
 use App\classes\Validator; 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class HeheController extends AbstractController
+{   
+   /**
+     * @Route("/", name="localhost", methods={"POST", "OPTIONS", "GET"})
+     */
+     public function localhost(): Response
 {
+$path = __DIR__ . '/../../public/index.html';
+$content = file_get_contents($path);
+return new Response($content);
+}
     /**
-     * @Route("/api/submit-form", name="submit_form", methods={"POST", "OPTIONS"})
+     * @Route("/api/submit-form/", name="submit_form", methods={"POST", "OPTIONS", "GET"})
      */
    public function submitForm(Request $request)
    {
        $response = new JsonResponse();
-       $response->headers->set('Access-Control-Allow-Origin', '*');
+      $response->headers->set('Access-Control-Allow-Origin', '*');
        $response->headers->set('Access-Control-Allow-Methods', 'POST, OPTIONS');
        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type');
 
@@ -29,14 +39,14 @@ class HeheController extends AbstractController
            return $this->createCorsResponse(['error' => 'Нет данных.'], 400);
        }
 
-       $database = new Database();
+    /*  $database = new Database();
        $connection = $database->conn;
-
+       
        $validator = new Validator($connection);
-
-       if (!$validator->validate($data)) {
+       
+      if (!$validator->validate($data)) {
            return $this->createCorsResponse(['errors' => $validator->errors], 400);
-       }
+       }*/
 
        $name = $data['fio'];
        $email = $data['email'];
